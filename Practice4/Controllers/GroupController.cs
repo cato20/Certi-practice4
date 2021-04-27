9266d1ba8e3323a731975d744df3a322b9c21409
cato20 <cae-10@hotmail.com>
2021-04-27 18:06:03 -0400

    P4-001 Initialized a dotnet core api project.

---
+++ Practice4/Controllers/GroupController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Practice4.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GroupController : ControllerBase
+    {
+
+
+
+    }
+}
