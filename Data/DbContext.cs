d46a1e5dbf3b288bbb409a66c5ee6679354b253f
cato20 <cae-10@hotmail.com>
2021-05-01 23:49:15 -0400

    P4-001 Create the layer of data and implemented the DbContext that has the crud for groups.

---
+++ Data/DbContext.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+using UPB.Practice4.Data.Models;
+
+namespace UPB.Practice4.Data
+{
+    public class DbContext
+    {
+        public List<Group> GroupTable { get; set; }
+        public DbContext()
+        {
+            GroupTable = new List<Group>();
+        }
+        public Group AddGroup(Group group)
+        {
+            GroupTable.Add(group);
+            return group;
+        }
+        public Group UpdateGroup(Group groupUpdate)
+        {
+            Group foundGroup = GroupTable.Find(group => group.Id == groupUpdate.Id);
+
+            foundGroup.Name = groupUpdate.Name;
+            foundGroup.Available = groupUpdate.Available;
+
+            return foundGroup;
+        }
+        public Group DeleteGroup(Group groupToDelete)
+        {
+            GroupTable.Remove(groupToDelete);
+            return groupToDelete;
+        }
+        public List<Group> GetAll()
+        {
+            return GroupTable;
+        }
+    }
+}
--- Data/Models/Group.cs
+++ Data/Models/Group.cs
@@ -1,12 +1,9 @@
-using System;
-
-namespace UPB.Practice4.Logic.Models
+namespace UPB.Practice4.Data.Models
 {
     public class Group
     {
         public String Id { get; set; }
         public String Name { get; set; }
         public int AvailableSlots { get; set; }
-
     }
 }
