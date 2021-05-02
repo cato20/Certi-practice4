6ebf29478546cc116f0c0b4d930e343b3e7702f6
cato20 <cae-10@hotmail.com>
2021-05-01 22:07:59 -0400

    P4-001 Created the manager and class for groups.

---
+++ Logic/Managers/GroupManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UPB.Practice4.Logic.Models;
+
+namespace UPB.Practice4.Logic.Managers
+{
+    public class GroupManager
+    {
+        public List<Group> GetAllGroups()
+        {
+            return new List<Group>();
+        }
+        public Group CreateGroup(string name, int availableSlots)
+        {
+            return new Group();
+        }
+        public Group UpdateGroup(Group group)
+        {
+            return new Group();
+        }
+        public Group DeleteGroup()
+        {
+            return new Group();
+        }
+
+
+    }
+
+}
---
+++ Logic/Models/Group.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UPB.Practice4.Logic.Models
+{
+    public class Group
+    {
+        public String Id { get; set; }
+        public String Name { get; set; }
+        public int Available { get; set; }
+
+    }
+}
